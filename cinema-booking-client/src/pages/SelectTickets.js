9a312fbae70c2119bd5b0b23645ee1e8d34c3428
jpharsh <jp.harsha@outlook.com>
2024-12-07 19:21:49 -0500

    Ava's work

--- cinema-booking-client/src/pages/SelectTickets.js
+++ cinema-booking-client/src/pages/SelectTickets.js
@@ -5,111 +5,134 @@ import './SelectTickets.css';
 import '../App.css';
 import { useLocation } from 'react-router-dom';
 
+
 const SelectTickets = () => {
-   const [adultCount, setAdultCount] = useState(0);
-   const [childCount, setChildCount] = useState(0);
-   const [seniorCount, setSeniorCount] = useState(0);
-   const navigate = useNavigate();
-   const location = useLocation();
-   const movie = location.state?.movie;
-   const showid = location.state?.showid;
-   const date = location.state?.date;
-   const time = location.state?.time;
-   const formattedSeats = location.state?.formattedSeats; 
-   const numSeats = formattedSeats.length;  
- 
-   if (!movie) {
-     return <p>No movie information available</p>;
-   }
-
-   const getTotalPrice = () => {
-       return (adultCount * 11.99) + (childCount * 9.99) + (seniorCount * 10.99);
-   };
+  const [adultCount, setAdultCount] = useState(0);
+  const [childCount, setChildCount] = useState(0);
+  const [seniorCount, setSeniorCount] = useState(0);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const movie = location.state?.movie;
+  const showid = location.state?.showid;
+  const date = location.state?.date;
+  const time = location.state?.time;
+  const formattedSeats = location.state?.formattedSeats;
+  const numSeats = formattedSeats.length; 
+  if (!movie) {
+    return <p>No movie information available</p>;
+  }
 
 
-   const handleIncreaseButton = (setCount) => {
-       setCount(prev => prev + 1);
-   };
+  const getTotalPrice = () => {
+      return (adultCount * 11.99) + (childCount * 9.99) + (seniorCount * 10.99);
+  };
 
 
-   const handleDecreaseButton = (count, setCount) => {
-       if (count > 0) setCount(prev => prev - 1);
-   };
 
 
-   const handleCancel = () => {
-       // Navigate back to the showtimes page
-       navigate('/select-seats', { state: { movie, showid, date, time } });
-   };
+  const handleIncreaseButton = (setCount) => {
+      setCount(prev => prev + 1);
+  };
 
 
-   // Sprint 3 Work
-   const getTotalTickets = () => adultCount + childCount + seniorCount;
 
 
-   const handleConfirmTickets = () => {
-       const ticketData = { adultCount, childCount, seniorCount };
-       navigate('/payment-info', { state: { ...ticketData, movie, totalPrice: getTotalPrice(), showid, date, time, formattedSeats } });
-   };
+  const handleDecreaseButton = (count, setCount) => {
+      if (count > 0) setCount(prev => prev - 1);
+  };
 
 
-   return (
-       <div className="App">
-           <div className="movie-section" style={{ width: '40%', alignContent: 'center', padding: '30px' }}>
-               <h2 style={{ textAlign: 'left' }}>Select {numSeats} Tickets</h2>
-               <div className="select-tickets-container">
-                   <img src={movie.poster_url || "https://via.placeholder.com/150"} className="movie-poster" style={{width: '100%' }}alt={`${movie.title} poster`} />
-                   <div className="ticket-selection-section">
-                       <p style={{margin: '10px 100px 20px 0px'}}>Standard (per ticket)</p>
-                       <div className="ticket-type">
-                           <p>Adult</p>
-                           <p>$11.99</p>
-                           <div>
-                               <button className="button" onClick={() => handleDecreaseButton(adultCount, setAdultCount)}>-</button>
-                               <span style={{ margin: '0 20px' }}>{adultCount}</span>
-                               <button className="button" onClick={() => handleIncreaseButton(setAdultCount)}>+</button>
-                           </div>
-                       </div>
 
 
-                       <div className="ticket-type">
-                           <p>Child</p>
-                           <p>$9.99</p>
-                           <div>
-                               <button className="button" onClick={() => handleDecreaseButton(childCount, setChildCount)}>-</button>
-                               <span style={{ margin: '0 20px' }}>{childCount}</span>
-                               <button className="button" onClick={() => handleIncreaseButton(setChildCount)}>+</button>
-                           </div>
-                       </div>
+  const handleCancel = () => {
+      // Navigate back to the showtimes page
+      navigate('/select-seats', { state: { movie, showid, date, time } });
+  };
 
 
-                       <div className="ticket-type">
-                           <p>Senior</p>
-                           <p>$10.99</p>
-                           <div>
-                               <button className="button" onClick={() => handleDecreaseButton(seniorCount, setSeniorCount)}>-</button>
-                               <span style={{ margin: '0 20px' }}>{seniorCount}</span>
-                               <button className="button" onClick={() => handleIncreaseButton(setSeniorCount)}>+</button>
-                           </div>
-                       </div>
 
 
-                       <h3 className="btn-container" style={{ padding: '30px 10px' }}>Total: ${getTotalPrice().toFixed(2)}</h3>
-                   </div>
-               </div>
+  // Sprint 3 Work
+  const getTotalTickets = () => adultCount + childCount + seniorCount;
 
 
-               <div className="btn-container">
-                 <button className="btn white" onClick={handleCancel}>Cancel</button>
-               <button
-               className="btn red"
-               onClick={handleConfirmTickets}
-               disabled={getTotalTickets() !== numSeats }>Confirm Tickets</button>
-               </div>
-           </div>
-       </div>
-   );
+
+
+  const handleConfirmTickets = () => {
+      const ticketData = { adultCount, childCount, seniorCount };
+      navigate('/payment-info', { state: { ...ticketData, movie, totalPrice: getTotalPrice(), showid, date, time, formattedSeats } });
+  };
+
+
+
+
+  return (
+      <div className="App">
+          <div className="movie-section" style={{ width: '40%', alignContent: 'center', padding: '30px' }}>
+              <h2 style={{ textAlign: 'left' }}>Select {numSeats} Tickets</h2>
+              <div className="select-tickets-container">
+                  <img src={movie.poster_url || "https://via.placeholder.com/150"} className="movie-poster" style={{width: '100%' }}alt={`${movie.title} poster`} />
+                  <div className="ticket-selection-section">
+                      <p style={{margin: '10px 100px 20px 0px'}}>Standard (per ticket)</p>
+                      <div className="ticket-type">
+                          <p>Adult</p>
+                          <p>$11.99</p>
+                          <div>
+                              <button className="button" onClick={() => handleDecreaseButton(adultCount, setAdultCount)}>-</button>
+                              <span style={{ margin: '0 20px' }}>{adultCount}</span>
+                              <button className="button" onClick={() => handleIncreaseButton(setAdultCount)}>+</button>
+                          </div>
+                      </div>
+
+
+
+
+                      <div className="ticket-type">
+                          <p>Child</p>
+                          <p>$9.99</p>
+                          <div>
+                              <button className="button" onClick={() => handleDecreaseButton(childCount, setChildCount)}>-</button>
+                              <span style={{ margin: '0 20px' }}>{childCount}</span>
+                              <button className="button" onClick={() => handleIncreaseButton(setChildCount)}>+</button>
+                          </div>
+                      </div>
+
+
+
+
+                      <div className="ticket-type">
+                          <p>Senior</p>
+                          <p>$10.99</p>
+                          <div>
+                              <button className="button" onClick={() => handleDecreaseButton(seniorCount, setSeniorCount)}>-</button>
+                              <span style={{ margin: '0 20px' }}>{seniorCount}</span>
+                              <button className="button" onClick={() => handleIncreaseButton(setSeniorCount)}>+</button>
+                          </div>
+                      </div>
+
+
+
+
+                      <h3 className="btn-container" style={{ padding: '30px 10px' }}>Total: ${getTotalPrice().toFixed(2)}</h3>
+                  </div>
+              </div>
+
+
+
+
+              <div className="btn-container">
+                <button className="btn white" onClick={handleCancel}>Cancel</button>
+              <button
+              className="btn red"
+              onClick={handleConfirmTickets}
+              disabled={getTotalTickets() !== numSeats }>Confirm Tickets</button>
+              </div>
+          </div>
+      </div>
+  );
 };
+
+
 
 
 export default SelectTickets;

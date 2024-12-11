2441db90172e1097a09505d9c86266f619bd67e3
Japachey <jspgold@gmail.com>
2024-12-11 03:00:45 -0500

    finish the ordersummary

--- cinema-booking-client/src/pages/PaymentInfo.js
+++ cinema-booking-client/src/pages/PaymentInfo.js
@@ -26,6 +26,7 @@ const PaymentInfo = () => {
   const [promoCode, setPromoCode] = useState("");
   const [discountedPrice, setDiscountedPrice] = useState(totalPrice);
   const [promoError, setPromoError] = useState("");
+  const [promoId, setPromoId] = useState("");
 
   const seats = formattedSeats.map((seat) => {
     const rowLetter = String.fromCharCode(65 + seat.row); // Convert row index to letter (A = 65 in ASCII)
@@ -613,6 +614,8 @@ const PaymentInfo = () => {
                   movie,
                   userSeats,
                   showid,
+                  selectedCard,
+                  promoCode
                 },
               });
             }}
